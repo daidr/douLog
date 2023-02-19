c8edf50965a27275b9b2666dabe7f02f2af5e96f
daidr <daidr@daidr.me>
2023-02-19 18:45:02 +0800

    feat: geometric background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,21 @@
 <script setup>
-const randomBackground = useState('randomBackground', () => {
-  const randomIndex = Math.floor(Math.random() * 2)
-  return randomIndex
-})
+// const randomBackground = useState('randomBackground', () => {
+//   const randomIndex = Math.floor(Math.random() * 2)
+//   return randomIndex
+// })
 
 const UiBackgroundGeometric = defineAsyncComponent(() =>
   import('@/components/ui/Background/Geometric.vue')
 )
 
-const UiBackgroundLifeGame = defineAsyncComponent(() =>
-  import('@/components/ui/Background/LifeGame.vue')
-)
+// const UiBackgroundLifeGame = defineAsyncComponent(() =>
+//   import('@/components/ui/Background/LifeGame.vue')
+// )
 </script>
 
 <template>
   <div class="main-wrapper">
-    <Component
-      :is="
-        randomBackground === 0 ? UiBackgroundGeometric : UiBackgroundLifeGame
-      "
-    />
+    <UiBackgroundGeometric />
     <slot />
     <SiteFooter />
   </div>
