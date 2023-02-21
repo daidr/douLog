fe15dd5406fc6e644da4aca9e69c1455e7d26fb1
daidr <daidr@daidr.me>
2023-02-21 21:56:24 +0800

    feat: resizeable avatar

--- components/ui/MainAvatar.vue
+++ components/ui/MainAvatar.vue
@@ -1,22 +1,53 @@
 <script setup lang="ts">
-defineProps<{
-  isActive: boolean
-}>()
+defineProps({
+  hoverable: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: Number,
+    default: 172,
+  },
+  stroke: {
+    type: Number,
+    default: 10,
+  },
+})
+
+const path = (d: string) => {
+  return `path('${d.replace(/\s+/g, ' ').trim()}')`
+}
 </script>
 
 <template>
-  <div class="avatar" :class="{ active: isActive }">
-    <svg view-box="0 0 172 172" width="172px" height="172px">
+  <div
+    class="avatar"
+    :class="{ hoverable: hoverable }"
+    :style="{
+      '--custom-size': `${size}px`,
+      '--custom-stroke': `${stroke}px`,
+      '--custom-clip-path': path(`
+        M 0 -${size / 2}
+        L 0 ${size / 2 - stroke}
+        A ${size / 2 - stroke} ${size / 2 - stroke} 0 0 0 ${
+        size - stroke * 2
+      } ${size / 2 - stroke}
+        L ${size - stroke * 2} -${size / 2}
+        Z`),
+    }"
+  >
+    <svg
+      :view-box="`0 0 ${size} ${size}`"
+      :width="`${size}px`"
+      :height="`${size}px`"
+    >
       <circle
-        cx="86"
-        cy="86"
-        r="80"
-        stroke-width="10"
+        :cx="`${size / 2}px`"
+        :cy="`${size / 2}px`"
+        :r="(size - stroke) / 2"
+        :stroke-width="stroke"
         class="stroke-primary-light"
         fill="none"
-        stroke-linecap="round"
-        stroke-dasharray="390, 502"
-        transform="rotate(-38 86 86)"
       ></circle>
     </svg>
     <UiLazyImage src="/images/avatar.png" alt="AVATAR" />
@@ -26,37 +57,35 @@ defineProps<{
 <style scoped lang="scss">
 .avatar {
   @apply select-none;
-  @apply "w-40 h-40";
   @apply relative;
+  --calc-size: calc(var(--custom-size) - 2 * var(--custom-stroke));
+  @apply w-[var(--calc-size)] h-[var(--calc-size)];
 
-  &.active {
+  &.hoverable {
     @apply pointer-events-none;
-
-    :deep(.lazy-image img) {
-      @apply scale-105 translate-y-0;
-    }
   }
 
   :deep(.lazy-image) {
-    width: 192px;
-    height: 192px;
-    transform: translate(-16px, -32px);
-    clip-path: path('M 22 0 L 22 107 A 6 6 0 0 0 170 120 L 170 0 Z');
+    @apply w-full h-full overflow-visible;
+    clip-path: var(--custom-clip-path);
 
     img {
-      @apply transform-gpu transition-transform scale-80 translate-y-16px;
-      @apply duration-650;
+      @apply transform-gpu transition-transform;
+      //   @apply  scale-130 translate-y-[calc(var(--custom-size)/20)];
+      @apply scale-135 translate-y-[calc(-1*var(--custom-size)/10)];
+      @apply duration-650 pointer-events-none;
       transition-timing-function: cubic-bezier(0.36, 1.1, 0.2, 1.2);
+    }
 
-      &:hover {
-        @apply scale-105 translate-y-0;
-      }
+    &:hover img {
+      @apply scale-145;
     }
   }
 
   svg {
     @apply absolute top-0 left-0;
-    @apply transform-gpu -translate-x-6px -translate-y-6px;
+    @apply transform-gpu pointer-events-none;
+    @apply translate-x-[calc(var(--custom-stroke)*-1)] translate-y-[calc(var(--custom-stroke)*-1)];
   }
 }
 </style>
