f08b742bbb90e1e3768d27cead3e4a751724e452
daidr <daidr@daidr.me>
2023-02-21 21:55:56 +0800

    feat: add articleDescription

--- config/base.js
+++ config/base.js
@@ -3,6 +3,7 @@ export const CONFIG = {
   enName: 'DaiDR',
   siteName: '戴兜的小屋',
   description: 'Coding the world.',
+  articleDescription: '前端练习生，喜欢折腾各种新鲜的东西。',
   keywords: [
     '博客',
     '前端',
