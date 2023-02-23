0629fa2931bb3da26cd68298455b581b469b56b1
daidr <daidr@daidr.me>
2023-02-23 23:40:48 +0800

    feat: article list support

---
+++ components/site/ArticleItem.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import type { IArticleListItem } from '~~/server/api/articles'
+import IconEye from '~icons/icon-park-outline/preview-open'
+import IconComment from '~icons/icon-park-outline/comment'
+
+defineProps<{ article: IArticleListItem }>()
+</script>
+
+<template>
+  <a class="article-item" target="_blank" :href="article.link">
+    <div class="left">
+      <h3 class="title">{{ article.title }}</h3>
+      <div class="details">
+        <div class="detail-item">{{ article.date }}</div>
+        <div v-if="article.tags.length" class="detail-item">
+          <span
+            v-for="tag of article.tags.slice(0, 1)"
+            :key="tag"
+            class="tag-item"
+            >#{{ tag }}</span
+          >
+        </div>
+        <div class="detail-item">
+          <IconEye />
+          <span>{{ article.viewCount }}</span>
+        </div>
+        <div class="detail-item">
+          <IconComment />
+          <span>{{ article.commentCount }}</span>
+        </div>
+      </div>
+      <div class="summary" v-html="article.excerpt"></div>
+    </div>
+    <div v-if="article.image" class="right">
+      <UiLazyImage :src="article.image" />
+    </div>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.article-item {
+  @apply flex-col md:flex-row flex justify-between rounded-2xl p-2;
+  @apply duration-500 transition transform;
+  transition-timing-function: cubic-bezier(0.36, 1.1, 0.2, 1.2);
+
+  &:hover {
+    @apply -translate-y-2;
+    @apply bg-primary-extralight/50;
+    @apply shadow-lg shadow-primary/10;
+  }
+
+  .left {
+    @apply z-1;
+
+    .title {
+      @apply text-2xl font-bold text-primary;
+    }
+
+    .details {
+      @apply flex items-center text-primary-medium/80 text-sm space-x-3;
+
+      .detail-item {
+        @apply flex items-center space-x-1;
+      }
+    }
+    .summary {
+      @apply mt-2 overflow-hidden overflow-ellipsis text-primary-medium/90;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 3;
+    }
+  }
+
+  .right {
+    @apply flex-grow-0 flex-shrink-0;
+    @apply lg:ml-2;
+    @apply w-240px h-163px;
+
+    @screen <md {
+      @apply mt-2 w-full;
+    }
+
+    :deep(img) {
+      @apply rounded-xl object-cover;
+    }
+  }
+}
+</style>
--- pages/blog/index/index.vue
+++ pages/blog/index/index.vue
@@ -1,6 +1,42 @@
 <script setup lang="ts">
 import IconAlarm from '~icons/icon-park-outline/alarm'
 import { CONFIG } from '~~/config/base'
+
+const isFetching = ref(false)
+const isEnded = ref(false)
+
+const fetchList = async (page: number = 1) => {
+  isFetching.value = true
+  const { data: _articleList } = await useFetch('/api/articles', {
+    query: {
+      page,
+    },
+  })
+  isFetching.value = false
+
+  return _articleList.value!
+}
+
+const articles = await fetchList()
+
+if (articles.length < 10) {
+  isEnded.value = true
+}
+
+const requestFetch = async () => {
+  if (isEnded.value || isFetching.value) return
+  const _articleList = await fetchList(articles.length / 10 + 1)
+  if (_articleList.length < 10) {
+    isEnded.value = true
+  }
+  articles.push(..._articleList)
+}
+
+const loadMoreRef = ref(null) as Ref<HTMLDivElement | null>
+
+useIntersectionObserver(loadMoreRef, async ([{ isIntersecting }]) => {
+  if (isIntersecting) requestFetch()
+})
 </script>
 
 <template>
@@ -9,12 +45,28 @@ import { CONFIG } from '~~/config/base'
       <IconAlarm />
       <p>{{ CONFIG.blogAnnouncement }}</p>
     </div>
+    <div class="article-list">
+      <SiteArticleItem
+        v-for="article of articles.filter(i => i.format === 'standard')"
+        :key="article.id"
+        :article="article"
+      />
+      <div
+        v-if="!isEnded"
+        ref="loadMoreRef"
+        class="load-more"
+        :class="{ disabled: isFetching }"
+        @click="requestFetch"
+      >
+        {{ isFetching ? '加载中...' : '加载更多' }}
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .content-wrapper {
-  @apply w-full flex flex-col items-stretch;
+  @apply w-full flex flex-col items-stretch space-y-3;
 
   .announcement-item {
     @apply w-full bg-white px-5 md:px-8 py-4 w-full;
@@ -25,6 +77,28 @@ import { CONFIG } from '~~/config/base'
 
     p {
       @apply text-primary/80;
+    }
+  }
+
+  .article-list {
+    @apply w-full bg-white w-full;
+    @apply px-5 py-5 sm:(px-8 py-8);
+    @apply rounded-4xl;
+    @apply shadow-2xl shadow-primary/30;
+    @apply flex flex-col space-y-4;
+  }
+
+  .load-more {
+    @apply w-full text-center text-base text-primary/90;
+    @apply cursor-pointer bg-primary-extralight/50;
+    @apply py-2 transition-colors rounded-xl;
+
+    &:hover {
+      @apply bg-primary-extralight/80;
+    }
+
+    &.disabled {
+      @apply pointer-events-none;
     }
   }
 }
