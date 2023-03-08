443f1814c9f9c87acde9c113b1875eb020058335
daidr <daidr@daidr.me>
2023-03-08 20:24:23 +0800

    feat: update background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,25 @@
 <script setup>
-// const randomBackground = useState('randomBackground', () => {
-//   const randomIndex = Math.floor(Math.random() * 2)
-//   return randomIndex
-// })
+const randomBackground = useState('randomBackground', () => {
+  const randomIndex = Math.floor(Math.random() * 2)
+  return randomIndex
+})
 
 const UiBackgroundGeometric = defineAsyncComponent(() =>
   import('@/components/ui/Background/Geometric.vue')
 )
 
-// const UiBackgroundLifeGame = defineAsyncComponent(() =>
-//   import('@/components/ui/Background/LifeGame.vue')
-// )
+const UiBackgroundLifeGame = defineAsyncComponent(() =>
+  import('@/components/ui/Background/LifeGame.vue')
+)
 </script>
 
 <template>
   <div class="main-wrapper">
-    <UiBackgroundGeometric />
+    <Component
+      :is="
+        randomBackground === 0 ? UiBackgroundGeometric : UiBackgroundLifeGame
+      "
+    />
     <slot />
     <SiteFooter />
   </div>
