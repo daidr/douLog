606e1dcc97a1fa1e53fc159034d99aaf945c6f56
daidr <daidr@daidr.me>
2023-03-10 01:54:25 +0800

    feat: code highlight support

--- components/common/ArticleRender.vue
+++ components/common/ArticleRender.vue
@@ -1,13 +1,37 @@
 <script setup lang="ts">
+import Prism from 'prismjs'
+
 export interface ICatalogItem {
   key: string
   title: string
   level: number
 }
 
-defineProps<{
+const props = defineProps<{
   articleHtml: string
 }>()
+
+const WARN_ICON =
+  '<svg xmlns="http://www.w3.org/2000/svg" class="mdui-icon" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 16a1 1 0 1 0 1 1a1 1 0 0 0-1-1Zm10.67 1.47l-8.05-14a3 3 0 0 0-5.24 0l-8 14A3 3 0 0 0 3.94 22h16.12a3 3 0 0 0 2.61-4.53Zm-1.73 2a1 1 0 0 1-.88.51H3.94a1 1 0 0 1-.88-.51a1 1 0 0 1 0-1l8-14a1 1 0 0 1 1.78 0l8.05 14a1 1 0 0 1 .05 1.02ZM12 8a1 1 0 0 0-1 1v4a1 1 0 0 0 2 0V9a1 1 0 0 0-1-1Z"/></svg>'
+
+const _articleHtml = computed(() => {
+  return (
+    props.articleHtml
+      // 替换警告图标
+      .replaceAll(
+        '<i class="mdui-icon material-icons">warning</i> <br/>',
+        WARN_ICON
+      )
+      // 让表格包裹一个容器
+      .replaceAll(
+        /(<table id="tablepress-\d+" class="tablepress tablepress-id-\d+">[\s\S]*<\/table>)/g,
+        (_: any, p1: any) => {
+          return `<div class="table-container">${p1}</div>`
+        }
+      )
+  )
+})
+
 const emit = defineEmits<{
   (event: 'generate-catalog', payload: ICatalogItem[]): void
   (event: 'active-title', payload: string | null): void
@@ -38,6 +62,7 @@ onMounted(() => {
     requestAnimationFrame(checkActiveTitle)
     articlePageWrapper!.addEventListener('scroll', onScroll)
   }
+  Prism.highlightAll()
 })
 
 onUnmounted(() => {
@@ -95,7 +120,7 @@ function isTitleActive(
 </script>
 
 <template>
-  <article class="blog-article-wrapper" v-html="articleHtml"></article>
+  <article class="blog-article-wrapper" v-html="_articleHtml"></article>
 </template>
 
 <style lang="scss" scoped>
@@ -110,6 +135,17 @@ article.blog-article-wrapper {
       @apply absolute top-0 left-0 bottom-0 w-1.5;
       @apply bg-primary-medium rounded-full;
       content: '';
+    }
+
+    .mdui-icon {
+      @apply text-xl;
+    }
+  }
+
+  :deep(blockquote.mdx-warning) {
+    @apply text-red-700/80;
+    &:before {
+      @apply bg-red-700/80;
     }
   }
 
@@ -139,6 +175,96 @@ article.blog-article-wrapper {
 
   :deep(mark) {
     @apply text-primary-medium;
+  }
+
+  :deep(.wp-block-code) {
+    @apply rounded-2xl;
+  }
+
+  :deep(code:not([class*='language-'])) {
+    @apply bg-primary/10 px-1 rounded-md text-primary;
+  }
+
+  :deep(.toolbar) {
+    @apply top-[0.5em] right-[0.5em] text-base;
+    @apply flex items-start justify-end space-x-1;
+
+    .toolbar-item {
+      @apply flex items-start select-none;
+
+      & > span {
+        @apply pointer-events-none;
+      }
+
+      & > * {
+        @apply bg-white shadow-none text-primary-medium/80;
+
+        &:hover,
+        &:focus {
+          @apply text-primary;
+        }
+      }
+    }
+
+    .copy-to-clipboard-button {
+      @apply cursor-pointer text-base;
+    }
+  }
+
+  :deep(.wp-block-code) {
+    @apply bg-primary-dark;
+  }
+
+  :deep(.table-container),
+  :deep(.mdui-table-fluid) {
+    @apply border-2 border-primary-light;
+    @apply rounded-8px overflow-y-hidden;
+
+    table {
+      @apply m-0 border-none border-separate;
+      @apply relative w-full bg-white;
+      border-spacing: 0;
+
+      thead {
+        @apply bg-primary-extralight/80;
+      }
+
+      td,
+      th {
+        @apply relative box-border;
+        @apply px-28px py-12px;
+        @apply text-left align-middle;
+      }
+
+      th {
+        @apply text-primary font-bold text-sm leading-32px;
+        @apply overflow-hidden whitespace-nowrap overflow-ellipsis;
+      }
+
+      td:first-child,
+      th:first-child {
+        @apply pr-0 pl-24px;
+      }
+
+      tbody td,
+      tbody th,
+      thead th {
+        @apply border-b-2 border-primary-light;
+      }
+
+      tbody tr {
+        @apply transition-colors relative;
+      }
+
+      tbody tr:hover {
+        @apply bg-primary-extralight/50;
+      }
+
+      tbody tr:last-child td,
+      tbody tr:last-child th {
+        @apply border-none;
+      }
+    }
   }
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import prismjs from 'vite-plugin-prismjs'
+
 export default defineNuxtConfig({
   typescript: {
     shim: false,
@@ -12,5 +14,23 @@ export default defineNuxtConfig({
   css: ['~/assets/scss/global.scss', '~/assets/fonts/BEYNO/font.css'],
   runtimeConfig: {
     apiEntry: process.env.NUXT_BLOG_API_ENTRY || 'https://daidr.me',
+  },
+  vite: {
+    plugins: [
+      prismjs({
+        languages: [
+          'javascript',
+          'typescript',
+          'css',
+          'markup',
+          'bash',
+          'shell',
+          'json',
+        ],
+        plugins: ['toolbar', 'copy-to-clipboard', 'show-language'],
+        theme: 'okaidia',
+        css: true,
+      }),
+    ],
   },
 })
