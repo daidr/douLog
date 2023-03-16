41555cefc563d8dfeef89b47fc32310859a90792
daidr <daidr@daidr.me>
2023-03-17 04:07:40 +0800

    feat: add globPatterns

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
     ],
     registerType: 'autoUpdate',
     workbox: {
+      globPatterns: [],
       runtimeCaching: [
         {
           urlPattern: /^https:\/\/cdn\.daidr\.me\/.*/i,
