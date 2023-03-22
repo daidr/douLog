2d6b8c026a609545bd5ca6673820f072063c8f93
daidr <daidr@daidr.me>
2023-03-22 14:05:23 +0800

    chore: disable service worker in dev mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     ],
     registerType: 'autoUpdate',
     devOptions: {
-      enabled: true,
+      enabled: false,
       type: 'module',
     },
     workbox: {
