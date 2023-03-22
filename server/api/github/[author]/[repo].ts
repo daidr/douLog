64e006d06d8c73fe30a5eabcc4eefe24284e6a26
daidr <daidr@daidr.me>
2023-03-22 16:23:18 +0800

    feat: add github api

---
+++ server/api/github/[author]/[repo].ts
@@ -0,0 +1,46 @@
+const { GithubAPIEntry } = useRuntimeConfig()
+
+export interface IGithubRepoInfo {
+  id: number
+  name: string
+  link: string
+  owner: {
+    login: string
+    url: string
+  }
+  description?: string
+  stars: number
+  forks: number
+  homepage?: string
+}
+
+export default cachedEventHandler(
+  async event => {
+    const author = getRouterParam(event, 'author')
+    const repo = getRouterParam(event, 'repo')
+    try {
+      const result = (await $fetch(`/repos/${author}/${repo}`, {
+        baseURL: GithubAPIEntry,
+      })) as any
+      return {
+        id: result.id,
+        name: result.name,
+        link: result.html_url,
+        owner: {
+          login: result.owner.login,
+          url: result.owner.html_url,
+        },
+        description: result.description,
+        stars: result.stargazers_count,
+        forks: result.forks_count,
+        homepage: result.homepage,
+      } as IGithubRepoInfo
+    } catch (e) {
+      return 'not found'
+    }
+  },
+  {
+    swr: true,
+    maxAge: 60 * 30,
+  }
+)
