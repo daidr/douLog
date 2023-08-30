32455b3b82257627d36e385842303ff71657af55
daidr <daidr@daidr.me>
2023-08-30 22:20:35 +0800

    fix: avatar transition

--- components/ui/MainAvatar.vue
+++ components/ui/MainAvatar.vue
@@ -30,8 +30,8 @@ const path = (d: string) => {
         M 0 -${size / 2}
         L 0 ${size / 2 - stroke}
         A ${size / 2 - stroke} ${size / 2 - stroke} 0 0 0 ${
-        size - stroke * 2
-      } ${size / 2 - stroke}
+          size - stroke * 2
+        } ${size / 2 - stroke}
         L ${size - stroke * 2} -${size / 2}
         Z`),
     }"
@@ -50,7 +50,9 @@ const path = (d: string) => {
         fill="none"
       ></circle>
     </svg>
-    <UiLazyImage src="/images/avatar.png" alt="AVATAR" />
+    <div class="avatar-image">
+      <UiLazyImage src="/images/avatar.png" alt="AVATAR" />
+    </div>
   </div>
 </template>
 
@@ -65,20 +67,19 @@ const path = (d: string) => {
     @apply pointer-events-none;
   }
 
-  :deep(.lazy-image) {
+  .avatar-image {
     @apply w-full h-full overflow-visible;
     clip-path: var(--custom-clip-path);
 
-    img {
+    :deep(.lazy-image) {
       @apply transform-gpu transition-transform;
-      //   @apply  scale-130 translate-y-[calc(var(--custom-size)/20)];
       @apply scale-125 translate-y-[calc(-1*var(--custom-size)/15)];
       @apply duration-650 pointer-events-none;
-      transition-timing-function: cubic-bezier(0.36, 1.1, 0.2, 1.2);
-    }
+      transition-timing-function: cubic-bezier(0.36, 1.1, 0.2, 1.2) !important;
 
-    &:hover img {
-      @apply scale-145;
+      &:hover {
+        @apply scale-145;
+      }
     }
   }
 
