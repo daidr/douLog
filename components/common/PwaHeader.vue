a184589617c81a796b3c2c488124c63628e65720
Xuezhou Dai <daidr@daidr.me>
2024-02-29 05:52:16 +0000

    fix: backdrop bug

--- components/common/PwaHeader.vue
+++ components/common/PwaHeader.vue
@@ -6,7 +6,8 @@
 .pwa-header {
     @apply h-[env(safe-area-inset-top,0)] transform-gpu translate-z-202vh z-95;
     @apply fixed top-0 left-0 right-0 pointer-events-none;
-    @apply backdrop-filter backdrop-blur-xl;
+    -webkit-backdrop-filter: blur(24px);
+    backdrop-filter: blur(24px);
     --gradient: linear-gradient(to bottom, white 0%,white 40%, transparent 97%, transparent 100%);
     -webkit-mask: var(--gradient);
     mask: var(--gradient);
