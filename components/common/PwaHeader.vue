2c9970b82007434e3bb47f9efbeb9f336ffcf541
Xuezhou Dai <daidr@daidr.me>
2024-02-29 06:09:52 +0000

    fix: pwa header isn't visible on safari

--- components/common/PwaHeader.vue
+++ components/common/PwaHeader.vue
@@ -1,31 +1,16 @@
-<script setup>
-const isBlurBg = ref(false)
-onMounted(() => {
-  setTimeout(() => {
-    isBlurBg.value = true
-  }, 100)
-})
-</script>
-
 <template>
   <div
-    class="pwa-header" :class="{
-      'blur-bg': isBlurBg,
-    }"
+    class="pwa-header"
   />
 </template>
 
 <style scoped lang="scss">
 .pwa-header {
-    @apply h-[env(safe-area-inset-top, 0)] transform-gpu translate-z-202vh z-95;
-    @apply fixed top-0 left-0 right-0 pointer-events-none;
+    @apply h-[calc(env(safe-area-inset-top,0)+1px)] transform-gpu translate-z-202vh z-95;
+    @apply fixed -top-1px left-0 right-0 pointer-events-none;
+    @apply backdrop-blur-xl;
     --gradient: linear-gradient(to bottom, white 0%, white 40%, transparent 97%, transparent 100%);
     -webkit-mask: var(--gradient);
     mask: var(--gradient);
-
-    &.blur-bg {
-        -webkit-backdrop-filter: blur(24px);
-        backdrop-filter: blur(24px);
-    }
 }
 </style>
